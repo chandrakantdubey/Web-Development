e7f8b27900fa8e447227cf026ee26116df52a198
chandrakantdubey <919chandrakant@gmail.com>
2022-09-15 14:05:54 +0530

    starting NS assignments

--- front_end_cp/react_fundamentals/AirBnb/src/components/Navbar.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default Navbar(){
-    return(
-        <div>
-            <img src="../assets/chandrakant.jpg"/>
-        </div>
-    )
-}
---
+++ front_end_cp/react_fundamentals/react_1/src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <div className='navbar-1'>
+        <img src='./logo192.png' alt='logo' width='100px' />
+        <h2>ReactFacts</h2>
+      </div>
+
+      <p>React Project 1</p>
+    </div>
+  );
+};
+
+export default Navbar;
