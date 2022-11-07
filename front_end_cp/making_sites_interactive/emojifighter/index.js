635aa30a11d3306c91f88946571923b9f3dc9c4b
chandrakantdubey <919chandrakant@gmail.com>
2022-11-07 10:20:26 +0530

    solo project 1

--- front_end_cp/making_sites_interactive/emojifighter/index.js
+++ front_end_cp/making_sites_interactive/emojifighter/index.js
@@ -29,3 +29,18 @@ console.log(randNum());
 fightButton.addEventListener('click', function () {
   stageEl.textContent = `${fighters[randNum()]} VS ${fighters[randNum()]}`;
 });
+
+const button = document.createElement('button');
+let root = document.querySelector('.root');
+root.append(button);
+let count = 0;
+button.textContent = `Count : `;
+button.addEventListener('click', () => {
+  count++;
+  button.textContent = `Count : ${count}`;
+});
+
+function Hello() {
+  return <h2>Increase Count on match.</h2>;
+}
+ReactDOM.render(<Hello />, document.querySelector('.root1'));
--- front_end_cp/react_fundamentals/react_1/src/components/Navbar.js
+++ front_end_cp/react_fundamentals/react_1/src/components/Navbar.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 
 const Navbar = () => {
   return (
-    <div className='navbar'>
-      <div className='navbar-1'>
-        <img src='./logo192.png' alt='logo' width='100px' />
-        <h2>ReactFacts</h2>
+    <nav className='navbar'>
+      <div className='nav--left'>
+        <img src='./logo192.png' alt='logo' className='nav--logo' />
+        <h2 className='nav--title'>ReactFacts</h2>
       </div>
 
-      <p>React Project 1</p>
-    </div>
+      <p className='nav--right'>React Project - 1</p>
+    </nav>
   );
 };
 
