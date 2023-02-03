d7247319b2bb487173cfe0d68408282ca8d2e0d5
chandrakantdubey <919chandrakant@gmail.com>
2023-02-03 05:50:31 +0530

    day 34

---
+++ js/complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/script.js
@@ -0,0 +1,7 @@
+// importing module
+import './shoppingCart';
+console.log('Importing module');
+// console.log(shippingCost);
+
+// addtoCart('bread', 5);
+// console.log(totalPrice, totalQunatity);
---
+++ js/complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/shoppingCart.js
@@ -0,0 +1,14 @@
+// exporting module
+console.log('Exporting module');
+
+// const shippingCost = 10;
+// const cart = [];
+
+// export const addtoCart = function (prod, qty) {
+//   cart.push({ prod, qty });
+//   console.log(`${qty} ${prod} added to the cart`);
+// };
+
+// const totalPrice = 237;
+// const totalQunatity = 23;
+// export { totalPrice, totalQunatity };
