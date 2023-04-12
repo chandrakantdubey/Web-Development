1f2dc1fa25bde5a467e0bc1b8ef900842354a182
chandrakantdubey <919chandrakant@gmal.com>
2023-04-12 13:27:35 +0530

    okay so this is my second last commit I am not gone add more to this repo except few mini projects that will pushed at weekend

--- emojifighter/index.js
+++ emojifighter/index.js
@@ -1,46 +1,47 @@
 let fighters = [
-  '🐉',
-  '🐥',
-  '🐊',
-  '💩',
-  '🦍',
-  '🐢',
-  '🐩',
-  '🦭',
-  '🦀',
-  '🐝',
-  '🤖',
-  '🐘',
-  '🐸',
-  '🕷',
-  '🐆',
-  '🦕',
-  '🦁',
+  "🐉",
+  "🐥",
+  "🐊",
+  "💩",
+  "🦍",
+  "🐢",
+  "🐩",
+  "🦭",
+  "🦀",
+  "🐝",
+  "🤖",
+  "🐘",
+  "🐸",
+  "🕷",
+  "🐆",
+  "🦕",
+  "🦁",
 ];
 
-let stageEl = document.getElementById('stage');
-let fightButton = document.getElementById('fightButton');
+const button = document.createElement("button");
+let root = document.querySelector(".root");
+root.append(button);
+let count = 0;
+button.textContent = `Score : ${count}`;
+
+let stageEl = document.getElementById("stage");
+let fightButton = document.getElementById("fightButton");
 
 function randNum() {
   return Math.floor(Math.random() * 16 + 1);
 }
-console.log(randNum());
 
-fightButton.addEventListener('click', function () {
-  stageEl.textContent = `${fighters[randNum()]} VS ${fighters[randNum()]}`;
-});
-
-const button = document.createElement('button');
-let root = document.querySelector('.root');
-root.append(button);
-let count = 0;
-button.textContent = `Count : `;
-button.addEventListener('click', () => {
-  count++;
-  button.textContent = `Count : ${count}`;
+fightButton.addEventListener("click", function () {
+  let randNum1 = randNum();
+  let randNum2 = randNum();
+  stageEl.textContent = `${fighters[randNum1]} VS ${fighters[randNum2]}`;
+  if (randNum1 === randNum2) {
+    count++;
+    button.textContent = `Count : ${count}`;
+  }
 });
 
 function Hello() {
-  return <h2>Increase Count on match.</h2>;
+  return <h1>Score Increases on match</h1>;
 }
-ReactDOM.render(<Hello />, document.querySelector('.root1'));
+ReactDOM.render(<Hello />, document.querySelector(".root1"));
