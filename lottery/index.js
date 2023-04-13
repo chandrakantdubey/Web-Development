df55fa09eba21e2a46b14a73c5116db2ecdbff97
Chandrakant Dubey <97845994+chandrakantdubey@users.noreply.github.com>
2023-04-14 00:38:14 +0530

    lottery - short js mini project

---
+++ lottery/index.js
@@ -0,0 +1,20 @@
+function getLottoNumbers() {
+  const winningNums = [];
+  for (let i = 0; i < 6; i++) {
+    winningNums.push(Math.round(Math.random() * 100));
+  }
+  return winningNums;
+}
+
+function getWinningNumbersHtml() {
+  return getLottoNumbers()
+    .map(function (num) {
+      return `<div class="number">${num}</div>`;
+    })
+    .join("");
+}
+
+document.querySelector("button").addEventListener("click", () => {
+  document.getElementById("winning-numbers").innerHTML =
+    getWinningNumbersHtml();
+});
